c035944b88570ddbb0e6107317b2dd4cd5c00dd9
Guillaume Teirlinck <guillaume.teirlinck@gmail.com>
2016-09-16 10:54:02 +0800

    Add routes and views for algo datapoints

---
+++ app/scripts/services/strat-datapoints-factory.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+
+angular.module('backtesterclientApp')
+.factory('StratDatapointsService', ['$resource', 'serverEnpoint', function ($resource, serverEndpoint) {
+    var address = serverEndpoint + 'api/algodatapoints/:stratname/:cross/?lowerBound=:lowerBound&upperBound=:upperBound';
+
+    return $resource(address, {
+        stratname: 'stratname',
+        cross: 'cross',
+        lowerBound: 'lowerBound',
+        upperBound: 'upperBound'
+    });
+}])
+.factory('StratDatapointsTradedService', ['$resource', 'serverEnpoint', function ($resource, serverEndpoint) {
+    var address = serverEndpoint + 'api/algodatapoints/:stratname/:cross/traded/?lowerBound=:lowerBound&upperBound=:upperBound';
+
+    return $resource(address, {
+        stratname: 'stratname',
+        cross: 'cross'
+    });
+}])
+.factory('StratDatapointsLatestService', ['$resource', 'serverEnpoint', function ($resource, serverEndpoint) {
+    var address = serverEndpoint + 'api/algodatapoints/:stratname/:cross/latest/';
+
+    return $resource(address, {
+        stratname: 'stratname',
+        cross: 'cross'
+    });
+}]);
---
+++ app/scripts/services/strat-datapoints-stratnames-factory.js
@@ -0,0 +1,8 @@
+﻿'use strict';
+
+angular.module('backtesterclientApp')
+.factory('StratDatapointsStratnameService', ['$resource', 'serverEnpoint', function ($resource, serverEndpoint) {
+    var address = serverEndpoint + 'api/algodatapointsstratnames';
+
+    return $resource(address);
+}]);
