94c5e053afe854bffbaa96e2eeeb6ad4bbf66454
Guillaume Teirlinck <guillaume.teirlinck@gmail.com>
2016-11-13 14:09:13 +0800

    Bugfixes

--- app/scripts/directives/job-details-positions-directive.js
+++ app/scripts/directives/job-details-positions-directive.js
@@ -15,20 +15,18 @@ angular.module('backtesterclientApp')
             $scope.posChartData = [];
 
             for (var i = 0; i < $scope.backtestJobPositions.length; i++) {
-                for (var j = 0; j < $scope.backtestJobPositions[i].PositionSecurities.length; j++) {
-                    if ($scope.positionsByCross[$scope.backtestJobPositions[i].PositionSecurities[j].Cross]) {
-                        $scope.positionsByCross[$scope.backtestJobPositions[i].PositionSecurities[j].Cross].push({
-                            quantity: $scope.backtestJobPositions[i].PositionSecurities[j].PositionQuantity,
-                            cumulativePnl: $scope.backtestJobPositions[i].PositionSecurities[j].RealizedPnlUsd,
-                            timestamp: $scope.backtestJobPositions[i].Timestamp
-                        });
-                    } else {
-                        $scope.positionsByCross[$scope.backtestJobPositions[i].PositionSecurities[j].Cross] = [{
-                            quantity: $scope.backtestJobPositions[i].PositionSecurities[j].PositionQuantity,
-                            cumulativePnl: $scope.backtestJobPositions[i].PositionSecurities[j].RealizedPnlUsd,
-                            timestamp: $scope.backtestJobPositions[i].Timestamp
-                        }];
-                    }
+                if ($scope.positionsByCross[$scope.backtestJobPositions[i].Cross]) {
+                    $scope.positionsByCross[$scope.backtestJobPositions[i].Cross].push({
+                        quantity: $scope.backtestJobPositions[i].PositionQuantity,
+                        cumulativePnl: $scope.backtestJobPositions[i].RealizedPnlUsd,
+                        timestamp: $scope.backtestJobPositions[i].LastUpdate
+                    });
+                } else {
+                    $scope.positionsByCross[$scope.backtestJobPositions[i].Cross] = [{
+                        quantity: $scope.backtestJobPositions[i].PositionQuantity,
+                        cumulativePnl: $scope.backtestJobPositions[i].RealizedPnlUsd,
+                        timestamp: $scope.backtestJobPositions[i].LastUpdate
+                    }];
                 }
             }
 
